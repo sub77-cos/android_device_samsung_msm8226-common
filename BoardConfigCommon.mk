f080f8579d318ee0410a2f968674953eddfa9bc7
Aditya Upreti <aupreti72@gmail.com>
2016-09-04 19:26:47 +0530

    [DO NOT MERGE] ms013g:Disable sepolicy again

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,8 +107,8 @@ ifeq ($(HOST_OS),linux)
 endif
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#-include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
