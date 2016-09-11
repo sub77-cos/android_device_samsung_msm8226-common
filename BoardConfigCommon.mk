b2618df3c6de3501f20d7974419cdf5b244b4e7d
Aditya Upreti <aupreti72@gmail.com>
2016-09-11 14:53:50 +0530

    ms013g: Use Broadcom BT

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,7 +32,7 @@ BOARD_USES_ALSA_AUDIO := true
  
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
-#BOARD_HAVE_BLUETOOTH_QCOM := true
+BOARD_HAVE_BLUETOOTH_BCM := true
 BLUETOOTH_HCI_USE_MCT := true
 
 # Bootloader
